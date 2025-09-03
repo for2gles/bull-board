db6869f77f9030c58e360908f62db661a10e21ce
허윤 <for2gles@naver.com>
2025-09-03 21:35:16 +0900

    fix the wrong order of input

--- packages/api/src/handlers/cleanJob.ts
+++ packages/api/src/handlers/cleanJob.ts
@@ -1,27 +1,25 @@
-import {
-  BullBoardRequest,
-  ControllerHandlerReturnType,
-  QueueJob,
-} from '../../typings/app';
-import { jobProvider } from '../providers/job';
-import { queueProvider } from '../providers/queue';
+import {BullBoardRequest, ControllerHandlerReturnType, QueueJob,} from '../../typings/app';
+import {jobProvider} from '../providers/job';
+import {queueProvider} from '../providers/queue';
 
 async function cleanJob(
-  _req: BullBoardRequest,
-  job: QueueJob
+    _req: BullBoardRequest,
+    job: QueueJob
 ): Promise<ControllerHandlerReturnType> {
-  if (job.repeatJobKey && queue?.removeJobScheduler) {
-    const { queueName } = _req.params;
-    const queue = _req.queues.get(queueName);
-    await queue.removeJobScheduler(job.repeatJobKey);
-  } else {
-    await job.remove();
-  }
+    if (job.repeatJobKey) {
+        const {queueName} = _req.params;
+        const queue = _req.queues.get(queueName);
+        if (queue?.removeJobScheduler) {
+            await queue.removeJobScheduler(job.repeatJobKey);
+        }
+    } else {
+        await job.remove();
+    }
 
-  return {
-    status: 204,
-    body: {},
-  };
+    return {
+        status: 204,
+        body: {},
+    };
 }
 
 export const cleanJobHandler = queueProvider(jobProvider(cleanJob));
